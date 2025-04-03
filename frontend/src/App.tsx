8521182ad8f1cd4b65eae887126e673a59ddc05b
Arjun Ramesh Prajapati <arjunprajapati4102001@gmail.com>
2025-04-04 00:01:56 +0530

    update import home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
-import Home from "./components/Home";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./components/Home"));
 
 const App: React.FC = () => {
+
   return (
     <div className="bg-[#f7f7f9] text-black min-h-screen pb-6">
-      <Home />
+      <Suspense fallback={<div className="text-center mt-28">Loading your app...</div>}>
+        <Home/>
+      </Suspense>
     </div>
   );
 };
